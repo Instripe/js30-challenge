fe6e73f3af274e93062edf2c2d7f3f31e90a91b5
Arnas <mikelionis.a@gmail.com>
2019-03-27 11:16:37 +0200

    Fix transition problem and refactor code

--- Day02 - Clock/script.js
+++ Day02 - Clock/script.js
@@ -1,16 +1,29 @@
 /*jshint esversion: 6 */
+const hourHand = document.getElementsByClassName('hour-hand')[0];
+const minHand = document.getElementsByClassName('min-hand')[0];
+const secondHand = document.getElementsByClassName('second-hand')[0];
+
+// Temporary disables transition on the seconds hand (after full circle)
+function fixTransition(seconds) {
+  if (seconds === 0) {
+    secondHand.classList.add('notransition');
+  } else if (seconds === 1) {
+    secondHand.classList.remove('notransition');
+  }
+}
 
 function runClock() {
   const currentTime = new Date();
-  let hours = currentTime.getHours();
-  let minutes = currentTime.getMinutes();
-  let seconds = currentTime.getSeconds();
-  const hourHand = document.getElementsByClassName('hour-hand')[0];
-  const minHand = document.getElementsByClassName('min-hand')[0];
-  const secondHand = document.getElementsByClassName('second-hand')[0];
+
+  const hours = currentTime.getHours();
   hourHand.style.transform = `rotate(${90 + 360/12 * hours}deg)`;
+
+  const minutes = currentTime.getMinutes();
   minHand.style.transform = `rotate(${90 + 360/60 * minutes}deg)`;
+
+  const seconds = currentTime.getSeconds();
   secondHand.style.transform = `rotate(${90 + 360/60 * seconds}deg)`;
+  fixTransition(seconds);
 }
 
 setInterval(runClock, 1000);
