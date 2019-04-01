29b65baca84f18e4f82ec789e819748199241f86
Arnas <mikelionis.a@gmail.com>
2019-04-01 12:17:25 +0300

    Add all main functionality

--- Day07 - Array Methods 2/script.js
+++ Day07 - Array Methods 2/script.js
@@ -15,14 +15,27 @@ const comments = [
   { text: 'Nice Nice Nice!', id: 542328 }
 ];
 
+const currentDate = new Date();
+
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const areThereAdults = people.some( person =>
+  currentDate.getFullYear() - person.year >= 19);
+console.log(areThereAdults);
 // Array.prototype.every() // is everyone 19 or older?
+const areEveryoneAdult = people.every( person =>
+  currentDate.getFullYear() - person.year >= 19);
+console.log(areEveryoneAdult);
 
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const comment = comments.find( comment => comment.id === 823423 );
+console.log(comment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const commentIndex = comments.findIndex( comment => comment.id === 823423 );
+comments.splice(commentIndex, 1);
+console.table(comments);
