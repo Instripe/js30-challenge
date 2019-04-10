a2d79fbe4637c0cba6727ef176132e87cf0d5957
Arnas <mikelionis.a@gmail.com>
2019-04-10 12:20:03 +0300

    Add final sorting functionality

--- Day17 - Conditional Sorting/script.js
+++ Day17 - Conditional Sorting/script.js
@@ -1,6 +1,15 @@
+const ul = document.getElementById('bands');
 const bands = [
     'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil',
     'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State',
     'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
     'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
 ];
+
+bands.sort((a, b) => {
+    aFixed = a.replace(/^(the |an |a )/i, '');
+    bFixed = b.replace(/^(the |an |a )/i, '');
+    return (aFixed > bFixed) ? 1 : ((bFixed > aFixed) ? -1 : 0);
+});
+
+bands.forEach(band => ul.innerHTML += `<li>${band}</li>`);
