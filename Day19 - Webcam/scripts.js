ad384c85985ecaf74d9c452463c7529ef85a6265
Arnas <mikelionis.a@gmail.com>
2019-04-12 21:41:31 +0300

    Add last webcam effects

--- Day19 - Webcam/scripts.js
+++ Day19 - Webcam/scripts.js
@@ -24,6 +24,14 @@ function paintToCanvas() {
     // Return for easier cancellation
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+        // Take pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+        // Change their values
+          //pixels = redEffect(pixels);
+          //pixels = rgbSplit(pixels);
+          //pixels = greenScreen(pixels);
+        // Put them back
+        ctx.putImageData(pixels, 0, 0);
     }, 16);
 }
 
@@ -36,8 +44,52 @@ function takePhoto() {
     const link = document.createElement('a');
     link.href = data;
     link.setAttribute('download', 'picture');
-    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Picture" />`;
     strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i+0] = pixels.data[i+0] + 100; // Red
+    pixels.data[i+1] = pixels.data[i+1] - 50; // Green
+    pixels.data[i+2] = pixels.data[i+2] * 0.5; // Blue
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i-150] = pixels.data[i+0]; // Red
+    pixels.data[i+100] = pixels.data[i+1]; // Green
+    pixels.data[i-150] = pixels.data[i+2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+  }
+  return pixels;
 }
 
 video.addEventListener('canplay', paintToCanvas);
